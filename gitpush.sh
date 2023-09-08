38feafaec75c3af353b54437f6db42df44ab8615
Adam Young <adamtyoung@gmail.com>
2023-09-08 08:55:48 -0700

    Commit from shell

--- gitpush.sh
+++ gitpush.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # read projects current directory with $PWD
 cd /Users/adamtyoung/calendars
-git add --all
+git add -A
 git commit -m "Commit from shell"
 git push origin --all
 
